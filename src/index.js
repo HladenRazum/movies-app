3cfa9e50a52fe93ff714c60a5564ac2607b74901
Krasimir Kostadinov <krasimir.kostadinov.dev@gmail.com>
2024-05-10 17:13:20 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const multer = require('multer')
+const path = require('path')
+
+const app = express()
+const PORT = process.env.PORT || 7777
+
+app.get('/', (req, res) => {
+  res.send('Opa aaaaaa artiki')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`)
+})
